961e73921fc2cc25306fd6e3da581d69aa5e130e
unknown <j.poliveira90@outlook.com>
2023-02-08 17:16:57 -0500

    Re-installed needed json packages + Created Header file.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Header from './Components/Header/Header';
 
 function App() {
   return (
     <div>
-      <p>Flashcard App.</p>
+      <Header/>
     </div>
   );
 }
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+const Header = ({ }) => {
+    return (  
+        <h1>*img* FLashcard API</h1>
+    );
+}
+ 
+export default Header;
