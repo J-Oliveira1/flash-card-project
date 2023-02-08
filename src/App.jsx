f965988580aa8a35edf04aefd4a76c6d41beefb4
unknown <j.poliveira90@outlook.com>
2023-02-08 15:02:33 -0500

    Deleted unused files + Created Components Folder along with new Components needed for App + installed Axios to connect Backend Project with Frontend Project.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function App() {
+  return (
+    <div>
+      <p>Flashcard App.</p>
+    </div>
+  );
+}
+
+export default App;
