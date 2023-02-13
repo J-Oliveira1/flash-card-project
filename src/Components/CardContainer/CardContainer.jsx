1615d3d2dd0f8ef5c146a5c80bdf5ab16de64e75
unknown <j.poliveira90@outlook.com>
2023-02-13 01:22:30 -0500

    Added Function to Component CardContainer where user has ability to go to the next card + added card total for collection.

--- src/Components/CardContainer/CardContainer.jsx
+++ src/Components/CardContainer/CardContainer.jsx
@@ -1,13 +1,41 @@
+
+import { useState } from "react";
 import Card from "../Card/Card";
 
 
 const CardContainer = ({cards}) => {
+    const [index, setIndex] = useState(0);
     const cardList = cards.map((card) => {
         return <Card card={card}/>;
     });
+
+    const handlePrevious = () => {
+        if (index === 0){
+            setIndex(cardList.lenght -1);
+        }
+        else {
+            setIndex(index - 1)
+        }
+    }
+
+    const handleNext = () => {
+        if (index ===  cardList.lenght - 1 ) {
+            setIndex(0);
+        }
+        else {
+            setIndex(index + 1);
+        }
+
+
+    }
     return ( 
         <section>
-            <div>{cardList}</div>
+            <div >{cardList[index]}</div>
+            <div>
+                <div>{index+1}/{cardList.length}</div>
+                <button onClick={handlePrevious}>PREVIOUS</button>
+                <button onClick={handleNext}>NEXT</button>
+            </div>
         </section>
      );
 }
